0697a7d0d42f892bb318bfa9dbb1a8788e247a05
cliente <cliente@192.168.1.206>
2017-09-20 10:46:07 -0300

    secont commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Aula de manipulação de xml</title>
+    </head>
+    <body>
+        <?php
+        echo "Olá mundo";
+        ?>
+    </body>
+</html>
